5a0dc2ba2c1bc1579e2bde76796bccd1bcc050a6
FrancoisFarlan <francois.farlan@gmail.com>
2023-05-04 20:34:20 +0200

    adding login route and logic to app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -74,9 +74,39 @@ class App {
             }
         });
 
-        // this.express.post("/login", (req: Request, res: Response) => {
-        //
-        // });
+        this.express.post("/login", async (req: Request, res: Response) => {
+            // Our login logic starts here
+            try {
+                // Get user input
+                const {email, password} = req.body;
+
+                // Validate user input
+                if (!(email && password)) {
+                    res.status(400).send("All input is required");
+                }
+                // Validate if user exist in our database
+                const user = await UserModel.findOne({email});
+
+                if (user && (await bcrypt.compare(password, user.password))) {
+                    // Create and save user token
+                    user.token = jwt.sign(
+                        {user_id: user._id, email},
+                        process.env.JWT_TOKEN_KEY,
+                        {
+                            expiresIn: "2h",
+                        }
+                    );
+
+                    // user
+                    res.status(200).json(user);
+                } else {
+                    res.status(400).send("Invalid Credentials");
+                }
+
+            } catch (err) {
+                console.log(err);
+            }
+        });
 
         // handle undefined routes
         this.express.use("*", (req: Request, res: Response) => {
